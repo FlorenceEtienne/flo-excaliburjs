16175c82ecb7377c1da7ee5bac0bf2fe18e2d3b3
FloE <florence.etienne@mail.citytech.cuny.edu>
2024-11-19 22:38:48 +0000

    added ball and paddle actors

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,40 @@
-import { Engine } from "excalibur";
-import { Player } from "./player";
-import { loader } from "./resources";
-
-class Game extends Engine {
-    constructor() {
-      super({width: 800, height: 600});
-    }
-    initialize() {
-      const player = new Player();
-      this.add(player);
-
-      this.start(loader);
-    }
+import { DisplayMode, Engine, Color } from "excalibur";
+import { loader } from "./loader";
+import { Paddle } from "./paddle";
+import { Ball } from "./ball";
+//assignment - add two balls that move in different directions and recreate sprites from mock as
+//temperory Actors
+class MainGame extends Engine {
+  constructor() {
+    super({
+      width: 1920,
+      height: 1080,
+      displayMode: DisplayMode.FitScreen,
+      backgroundColor: Color.White,
+    });
   }
-  
-  export const game = new Game();
-  game.initialize();
+
+  initialize() {
+    const paddle1 = new Paddle({ x: 500, y: 500, colorOfBar: Color.Red });
+    this.add(paddle1);
+
+    const paddle2 = new Paddle({ x: 100, y: 100, colorOfBar: Color.Orange });
+    this.add(paddle2);
+
+    this.input.pointers.primary.on("move", (e) => {
+      paddle1.pos.x = e.worldPos.x;
+    });
+
+    const ball1 = new Ball({
+      x: 500,
+      y: 300,
+      colorOfBall: Color.Black,
+    });
+    this.add(ball1);
+
+    this.start(loader);
+  }
+}
+
+const game = new MainGame();
+game.initialize();
