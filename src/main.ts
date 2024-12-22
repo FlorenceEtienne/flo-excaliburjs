3de5763a284ee3b4604820f082f5763ac9eb4d58
FuzzWil <florence.etienne@mail.citytech.cuny.edu>
2024-12-21 22:24:49 -0500

    reworking final project, again

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { DisplayMode, Engine, Color } from "excalibur";
 import { loader } from "./resources";
 import { Player } from "./player";
-import { MonsterEggs } from "./monster";
 
 class MainGame extends Engine {
   constructor() {
@@ -16,46 +15,11 @@ class MainGame extends Engine {
   initialize() {
     const player = new Player();
 
-    const egg1 = new MonsterEggs({
-      x: 128,
-      y: 128,
-      width: 16,
-      height: 16,
-      //colorOfObject: Color.fromRGB(19, 55, 29)
-    });
-    this.add(egg1); 
-    
-    const egg2 = new MonsterEggs({
-      x: 500,
-      y: 128,
-      width: 16,
-      height: 16,
-      // colorOfObject: Color.fromRGB(19, 55, 29)
-    });
-    this.add(egg2);
-
-    //the goal is to go to Nok Noks and collect the Magic cores they stole
-    //(you got anti-magic abilities, which explains your time running out)
-
-    //by clicking on the Nok Noks reveals a Magic core for a moment
-    
-    //you get the Magic cores and then click the machine to get a point
-    //getting enough points, your supervisor gives you the win
-
-    //three npcs as three important townsfolk
-    //(the townsfolk are advanced Nok Noks that have been qurantined)
-    //a shopkeeper gives you a document
-    //a blacksmith gives you a document
-    //a street rat (their parent a baker) gives you a document
-
-    //by getting all documents you get a seperate ending where you're chilling
-    //at your place in peace
-
-    egg2.graphics.opacity = 0;
-    egg1.on("pointerdown", () => {
-      console.log("show on console");
-      egg2.graphics.opacity = 1;
-    })
+    // egg2.graphics.opacity = 0;
+    // egg1.on("pointerdown", () => {
+    //   console.log("show on console");
+    //   egg2.graphics.opacity = 1;
+    // })
 
     this.add(player);
     this.start(loader);
--- src/resources.ts
+++ src/resources.ts
@@ -1,12 +1,8 @@
 import { ImageSource, Loader } from "excalibur";
 import playerImage from './images/alexis_stivens.png'
-import monsterEggImage from './images/monster_egg.png'
-// import enemyImage from './images/nogitsune.png'
 
 export const Resources = {
   playerImage: new ImageSource(playerImage),
-  monsterEggImage: new ImageSource(monsterEggImage)
-//   enemyImage: new ImageSource(enemyImage)
 }
 
 export const loader = new Loader();
