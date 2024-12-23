abe6d92ee198791744b509a1f3690e4263a86c53
FuzzWil <florence.etienne@mail.citytech.cuny.edu>
2024-12-23 08:37:49 -0500

    removed files to rework project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { DisplayMode, Engine, Color } from "excalibur";
 import { loader } from "./resources";
-import { Player } from "./player";
+// import { Player } from "./player";
 
 class MainGame extends Engine {
   constructor() {
@@ -8,20 +8,20 @@ class MainGame extends Engine {
       width: 1920,
       height: 1080,
       displayMode: DisplayMode.FitScreen,
-      backgroundColor: Color.fromRGB(72,111,56),
+      backgroundColor: Color.DarkGray,
     });
   }
 
   initialize() {
-    const player = new Player();
+    // const player = new Player();
 
-    // egg2.graphics.opacity = 0;
-    // egg1.on("pointerdown", () => {
-    //   console.log("show on console");
-    //   egg2.graphics.opacity = 1;
-    // })
+    // // egg2.graphics.opacity = 0;
+    // // egg1.on("pointerdown", () => {
+    // //   console.log("show on console");
+    // //   egg2.graphics.opacity = 1;
+    // // })
 
-    this.add(player);
+    // this.add(player);
     this.start(loader);
 
   }
--- src/player.ts
+++ src/player.ts
@@ -1,52 +1,52 @@
-import { Actor, Engine, Color, Vector, vec, CollisionType } from "excalibur";
-import { startingPosition } from "./startingPosition";
-import { Resources } from "./resources";
+// import { Actor, Engine, Color, Vector, vec, CollisionType } from "excalibur";
+// import { startingPosition } from "./startingPosition";
+// import { Resources } from "./resources";
 
-export class Player extends Actor {
-    constructor() {
-        const position = startingPosition;
+// export class Player extends Actor {
+//     constructor() {
+//         const position = startingPosition;
 
-        super({
-            pos: position,
-            width: 16,
-            height: 16,
-            color: Color.fromRGB(0, 80, 80),
-            collisionType: CollisionType.Active
-        });
-    }
+//         super({
+//             pos: position,
+//             width: 16,
+//             height: 16,
+//             color: Color.fromRGB(0, 80, 80),
+//             collisionType: CollisionType.Active
+//         });
+//     }
 
-    lastPosition = startingPosition;
-    isMoving = false;
+//     lastPosition = startingPosition;
+//     isMoving = false;
 
-    private setLocationAsFollow(vec: Vector) {
-        this.lastPosition = vec;
-        this.isMoving = true;
-    }
+//     private setLocationAsFollow(vec: Vector) {
+//         this.lastPosition = vec;
+//         this.isMoving = true;
+//     }
 
-    //once the game initializes, the follower function will initialize with it
-    onInitialize(engine: Engine): void {
-        this.graphics.add(Resources.playerImage.toSprite());
+//     //once the game initializes, the follower function will initialize with it
+//     onInitialize(engine: Engine): void {
+//         this.graphics.add(Resources.playerImage.toSprite());
         
-        engine.input.pointers.primary.on('down', (event) => {
-            const clickedPosition = event.worldPos;
-            this.setLocationAsFollow(clickedPosition);
+//         engine.input.pointers.primary.on('down', (event) => {
+//             const clickedPosition = event.worldPos;
+//             this.setLocationAsFollow(clickedPosition);
 
-            this.actions
-            .easeTo(clickedPosition.x, clickedPosition.y, 1000) //position on the x axis, position on the y axis, duration in millisecs
-            .callMethod(() => {
-                this.isMoving = false;
-            });
-        });
-    }
+//             this.actions
+//             .easeTo(clickedPosition.x, clickedPosition.y, 1000) //position on the x axis, position on the y axis, duration in millisecs
+//             .callMethod(() => {
+//                 this.isMoving = false;
+//             });
+//         });
+//     }
 
-    onPostUpdate(): void {
-        if (!this.isMoving) {
-            this.stop();
-        }
-    }
+//     onPostUpdate(): void {
+//         if (!this.isMoving) {
+//             this.stop();
+//         }
+//     }
 
-    stop() {
-        this.vel = vec(0, 0); //velocity isn't there
-        this.acc = vec(0, 0); //acceleration isn't there
-    }
-}
+//     stop() {
+//         this.vel = vec(0, 0); //velocity isn't there
+//         this.acc = vec(0, 0); //acceleration isn't there
+//     }
+// }
